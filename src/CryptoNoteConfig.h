cf40fb52a84981fbd090905e654e54fded968fa7
Saci-de-bani <34216969+Saci-de-bani@users.noreply.github.com>
2018-01-23 22:40:50 -0500

    Update CryptoNoteConfig.h
    added genesis hex

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "koalacoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff000108029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101c09c76b897ca775cfb2a3f46fd487a51b8e0b9e9119145421f49f8207b4bf7ab";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
